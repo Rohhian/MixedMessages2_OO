a1aa84696171e60a556eec2518ba712b7e4e10b6
Kristian Miilen <kristianmiilen@gmail.com>
2024-05-03 12:38:36 +0300

    refactor setWords validation and dataentry if-blocks + fixed tablenames ckeking bug

--- src/Controllers/SetWordsController.php
+++ src/Controllers/SetWordsController.php
@@ -9,28 +9,21 @@ class SetWordsController {
     public function __construct() {
         $db = new Database();
 
-        $table = $_POST['table'];
-        $word = $_POST['word'];
+        $tableFromPost = $_POST['table'];
+        $wordFromPost = $_POST['word'];
 
-        if ($table != $_ENV['TABLE_ADJECTIVES'] || $_ENV['TABLE_NOUNS'] || $_ENV['TABLE_VERBS'] || $_ENV['TABLE_WHO']) {
-            echo json_encode("Wrong table name");
-        } elseif ($word == '') {
-            echo json_encode("Don't send empty strings");
-        } else {
-            $sanitizedObj = preg_replace('/[^a-zA-Z-]/', '', $word);
-            try {
-                $preparedSql = $db->getConnection()->prepare(
-                    "INSERT INTO $table (col2) VALUES (:word)"
-                );
-                $preparedSql->execute([':word' => $sanitizedObj]);
-                echo json_encode("Entry successful. Added " . $sanitizedObj);
-            } catch (Exception $e) {
-                if (str_contains($e->getMessage(), 'Duplicate entry')) {
-                    echo json_encode("Duplicate entry");
-                } else {
-                    echo json_encode($e->getMessage());
-                }
-            }
+        $tableNames = [$_ENV['TABLE_ADJECTIVES'], $_ENV['TABLE_NOUNS'], $_ENV['TABLE_VERBS'], $_ENV['TABLE_WHO']];
+
+        echo !in_array($tableFromPost, $tableNames) ? json_encode("Wrong table name") : null;
+        echo $wordFromPost == '' ? json_encode("Don't send empty strings") : null;
+
+        try {
+            $sanitizedObj = preg_replace('/[^a-zA-Z-]/', '', $wordFromPost);
+            $preparedSql = $db->getConnection()->prepare("INSERT INTO $tableFromPost (col2) VALUES (:wordFromPost)");
+            $preparedSql->execute([':wordFromPost' => $sanitizedObj]);
+            echo json_encode("Entry successful. Added " . $sanitizedObj);
+        } catch (Exception $e) {
+            echo str_contains($e->getMessage(), 'Duplicate entry') ? json_encode("Duplicate entry") : json_encode($e->getMessage());
         }
     }
 }
