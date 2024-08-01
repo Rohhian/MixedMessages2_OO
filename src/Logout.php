c9532de08361cf4240620ee7898a4429f5cf0f8d
Kristian Miilen <kristianmiilen@gmail.com>
2024-08-01 20:10:34 +0300

    refactor session handling and logout functionality

--- src/Logout.php
+++ src/Logout.php
@@ -4,12 +4,20 @@ namespace MixedMessages2;
 
 class Logout {
     public function __construct() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
         $_SESSION = array();
         session_destroy();
 
         if (isset($_COOKIE[session_name()])) {
             setcookie(session_name(), '', time() - 42000, '/');
         }
+
+        header('Content-Type: application/json');
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+        header('Pragma: no-cache');
 
         echo json_encode(['status' => 'success']);
     }
