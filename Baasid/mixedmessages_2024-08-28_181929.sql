05219fb92421f225a4ee2ae5677c07d4134b0356
Kristian Miilen <kristianmiilen@gmail.com>
2024-08-28 18:23:32 +0300

    basebase

--- Baasid/mixedmessages_2024-08-28_181929.sql
+++ Baasid/mixedmessages_2024-08-28_181929.sql
@@ -28,7 +28,7 @@ CREATE TABLE `adjectives` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `adjectives_id_uindex` (`id`),
   UNIQUE KEY `adjectives_col2_uindex` (`col2`) USING HASH
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `adjectives` (
 --
 
 /*!40000 ALTER TABLE `adjectives` DISABLE KEYS */;
-INSERT INTO `adjectives` VALUES (1,'red'),(2,'green'),(3,'violet'),(4,'blue'),(5,'yellow'),(6,'angry'),(7,'friendly'),(8,'heavy'),(9,'lightweight'),(10,'fat'),(11,'skinny'),(12,'shitty'),(13,'amazing'),(14,'sleepy'),(15,'sexy'),(16,'drunk'),(17,'sad'),(18,'happy'),(19,'fabulous'),(20,'unimaginative'),(21,'unbelievable'),(22,'unhappy'),(23,'normal'),(24,'crazy'),(25,'bipolar'),(26,'honest'),(27,'fearless'),(28,'fantastic'),(29,'smelly'),(30,'poor'),(31,'rich'),(32,'beautiful'),(33,'ugly'),(34,'smart');
+INSERT INTO `adjectives` VALUES (1,'red'),(2,'green'),(3,'violet'),(4,'blue'),(5,'yellow'),(6,'angry'),(7,'friendly'),(8,'heavy'),(9,'lightweight'),(10,'fat'),(11,'skinny'),(12,'shitty'),(13,'amazing'),(14,'sleepy'),(15,'sexy'),(16,'drunk'),(17,'sad'),(18,'happy'),(19,'fabulous'),(20,'unimaginative'),(21,'unbelievable'),(22,'unhappy'),(23,'normal'),(24,'crazy'),(25,'bipolar'),(26,'honest'),(27,'fearless'),(28,'fantastic'),(29,'smelly'),(30,'poor'),(31,'rich'),(32,'beautiful'),(33,'ugly'),(34,'smart'),(35,'stupid'),(36,'depressed'),(37,'violent'),(38,'furious');
 /*!40000 ALTER TABLE `adjectives` ENABLE KEYS */;
 
 --
@@ -52,7 +52,7 @@ CREATE TABLE `nouns` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `nouns_id_uindex` (`id`),
   UNIQUE KEY `nouns_col1_uindex` (`col2`) USING HASH
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,8 +60,34 @@ CREATE TABLE `nouns` (
 --
 
 /*!40000 ALTER TABLE `nouns` DISABLE KEYS */;
-INSERT INTO `nouns` VALUES (1,'car'),(2,'plane'),(3,'cat'),(4,'dog'),(5,'house'),(6,'bike'),(7,'road'),(8,'man'),(9,'woman'),(10,'child'),(11,'friend'),(12,'enemy'),(13,'student'),(14,'teacher'),(15,'politician'),(16,'gangsta'),(17,'doctor'),(18,'chemist'),(19,'developer'),(20,'wallet'),(21,'dancer'),(22,'pilot'),(23,'priest'),(24,'biker'),(25,'book'),(26,'monkey'),(27,'bird'),(28,'bullshitter'),(29,'variable'),(30,'yogi'),(31,'fan'),(32,'businessman'),(33,'soldier'),(34,'skunk'),(35,'businesswoman'),(36,'poor'),(37,'rich'),(43,'mother-in-law'),(44,'shit'),(45,'piss'),(46,'ipad'),(47,'horse'),(49,'chicken'),(50,'father-in-law'),(51,'holycow');
+INSERT INTO `nouns` VALUES (1,'car'),(2,'plane'),(3,'cat'),(4,'dog'),(5,'house'),(6,'bike'),(7,'road'),(8,'man'),(9,'woman'),(10,'child'),(11,'friend'),(12,'enemy'),(13,'student'),(14,'teacher'),(15,'politician'),(16,'gangsta'),(17,'doctor'),(18,'chemist'),(19,'developer'),(20,'wallet'),(21,'dancer'),(22,'pilot'),(23,'priest'),(24,'biker'),(25,'book'),(26,'monkey'),(27,'bird'),(28,'bullshitter'),(29,'variable'),(30,'yogi'),(31,'fan'),(32,'businessman'),(33,'soldier'),(34,'skunk'),(35,'businesswoman'),(36,'poor'),(37,'rich'),(43,'mother-in-law'),(46,'ipad'),(47,'horse'),(49,'chicken'),(50,'father-in-law'),(51,'holycow'),(52,'captain'),(53,'printer'),(54,'axe'),(62,'king'),(63,'queen'),(64,'spaceman'),(65,'submarine');
 /*!40000 ALTER TABLE `nouns` ENABLE KEYS */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` tinytext NOT NULL,
+  `last_name` tinytext NOT NULL,
+  `email` tinytext NOT NULL,
+  `pass` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`) USING HASH
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Kristian','Miilen','miilen1@miilen.com','asdasd'),(2,'Kristian','Miilen','miilen2@miilen.com','asdasd');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 --
 -- Table structure for table `verbs`
@@ -76,7 +102,7 @@ CREATE TABLE `verbs` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `verbs_id_uindex` (`id`),
   UNIQUE KEY `verbs_col2_uindex` (`col2`) USING HASH
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,7 +110,7 @@ CREATE TABLE `verbs` (
 --
 
 /*!40000 ALTER TABLE `verbs` DISABLE KEYS */;
-INSERT INTO `verbs` VALUES (1,'driving'),(2,'talking to'),(3,'singing to'),(4,'running away from'),(5,'loving'),(6,'jumping on'),(7,'avoiding'),(8,'walking towards'),(9,'screaming at'),(10,'digging a hole to'),(11,'apologizing to'),(12,'crawling towards'),(13,'painting on'),(14,'trying to understand'),(15,'talking about'),(16,'singing about'),(17,'eyeballing'),(18,'eating with'),(19,'bullshitting'),(20,'defining'),(21,'drinking with'),(22,'meditating on'),(23,'meditating with'),(24,'smelling'),(25,'swimming with'),(26,'playing with'),(27,'flying over'),(28,'reading'),(30,'praying'),(31,'humming');
+INSERT INTO `verbs` VALUES (1,'driving'),(2,'talking to'),(3,'singing to'),(4,'running away from'),(5,'loving'),(6,'jumping on'),(7,'avoiding'),(8,'walking towards'),(9,'screaming at'),(10,'digging a hole to'),(11,'apologizing to'),(12,'crawling towards'),(13,'painting on'),(14,'trying to understand'),(15,'talking about'),(16,'singing about'),(17,'eyeballing'),(18,'eating with'),(19,'bullshitting'),(20,'defining'),(21,'drinking with'),(22,'meditating on'),(23,'meditating with'),(24,'smelling'),(25,'swimming with'),(26,'playing with'),(27,'flying over'),(28,'reading'),(30,'praying'),(31,'humming'),(32,'selling'),(33,'flying');
 /*!40000 ALTER TABLE `verbs` ENABLE KEYS */;
 
 --
@@ -124,4 +150,4 @@ INSERT INTO `who` VALUES (1,'Your'),(2,'His'),(3,'Her'),(4,'Their'),(5,'Our');
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-21 15:14:52
+-- Dump completed on 2024-08-28 18:19:35
